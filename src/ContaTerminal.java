6e25517bb4bb275592ae65e1bc404f8bbd5e913f
Dev-Yuriko <yurikojavaprogrammer@gmail.com>
2023-09-04 11:33:19 -0300

    adicionando código a classe ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class App {
+import java.util.Scanner;
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite seu nome: ");
+        String nome =sc.next();
+
+        System.out.print("Por favor, digite o número da Agência !: ");
+        String agencia =sc.next();
+
+        System.out.print("Por favor, digite o número da conta !: ");
+        int numeroConta =sc.nextInt();
+
+        System.out.print("Por favor, digite o seu saldo !: ");
+        float saldo =sc.nextFloat();
+
+
+
+        System.out.println("Olá"+ nome + ", obrigada por criar uma conta em nosso banco, sua agência é " + agencia + "," 
+        + "conta" + numeroConta + "e seu saldo " + saldo + " já está disponível para saque");
+
+        
+        sc.close();
     }
 }
